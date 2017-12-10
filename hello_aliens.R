a2ef606f92489addb19570f2caffe965a108e890
joeygibli <joeygibli@gmail.com>
2017-12-10 17:55:14 -0500

    Upload R file

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+cat("Hello Aliens, 
+    If you are recieving this message then congratulations, you are speaking to alternate life forms. I am happy to be your introduction to the human race. While we continue to have societal problems, humankind is composed of many folk eager to make positive change. I was wondering if you had similar problems? What are your solutions to confront indifference and difficult members of society? Also, I was wondering what you ate and whether food plays an important role in your culture. Human culture is often times defined by their foods, and it a connection between people and somethign many love to share.\n
+    Thank you, hope to hear back from you soon!
+    Joey")
